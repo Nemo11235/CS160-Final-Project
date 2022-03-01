f2e57b6f0249b3357be0d641ea4bdc7f331c6463
James Yu <jamesyu200141@gmail.com>
2022-02-28 18:12:43 -0800

    Implemented input validator by making sure input is 5 letters long, and that all elements of the input are lowercase letters of the English alphabet. Also tried to get started on Allen's 3rd suggestion of the corner case by adding a dummy variable containing the sorted string of the input in inputCheck, but couldn't figure out how to fix the problem.

--- wordle.js
+++ wordle.js
@@ -6,9 +6,20 @@ N = incorrect letter
 */
 let word = 'apple';
 
+//function to check if the input is valid (5 letter alphabet)
+//later on, potentially implement dictionary so game only accepts valid English words
+function inputValidator(userInput) {
+    if(userInput.length==5 && /^[a-z]+$/.test(userInput)){
+        return true;
+    }
+    return false;
+}
+
 //function to create feedback based off of user's guess
 function inputCheck(userInput) {
     let inputResults = new Array();
+    let sortedInput = userInput.split('').sort().join('');
+
     for(let i = 0; i < word.length; i++) {
         if(userInput[i] == word[i]) {
             inputResults[i] = 'Y';
@@ -33,7 +44,12 @@ console.log("Welcome to the game!\n");
 
 for(let i = 0; i < 6; i++) {
     console.log("Attempt: " + (i+1) + '/6 ');
-    const input = prompt("Please enter your guess: ");
+
+    //while input is not valid, guess again
+    while(!inputValidator(input = prompt("Please enter your guess: "))){
+        console.log("Invalid Input")
+    }
+
     let answer = inputCheck(input);
     console.log('feedback: ');
     console.log(answer.join(''));
