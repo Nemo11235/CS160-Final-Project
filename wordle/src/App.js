3470e954eecbe5031bbe9333079090a3186f2a78
Allen Liu <liuzhexian11235@gmail.com>
2022-03-03 19:16:21 -0800

    implemented keyboard without functionality

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>This is a test</p>
-    </div>
-  );
+
+  
+  
+    return (
+      <div className="App">
+        <Button variant="primary">Primary</Button>{' '}
+        <Keyboard
+        onChange={input =>
+          null}
+        onKeyPress={button =>
+          null}
+      />
+      </div>
+
+    );
+  
 }
 
 export default App;
