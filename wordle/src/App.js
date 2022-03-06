b0f87151562b3da1a3b010006bf6747959cba3cb
Allen Liu <liuzhexian11235@gmail.com>
2022-03-05 23:12:14 -0800

    removed useless keys and changed all keys to uppercase

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import React, {Component} from 'react';
-import Button from 'react-bootstrap/Button';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 
 function App() {
-
-  
-  
     return (
       <div className="App">
-        <Button variant="primary">Primary</Button>{' '}
         <Keyboard
         onChange={input =>
           null}
         onKeyPress={button =>
           null}
+          layout={{
+            'default': [
+              'Q W E R T Y U I O P',
+              'A S D F G H J K L',
+              '{bksp} Z X C V B N M {enter}',
+            ]
+          }}
       />
       </div>
 
