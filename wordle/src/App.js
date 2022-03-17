4fcbdf3f5169fc4cd4744288e08829ae60ccd3ab
Allen Liu <liuzhexian11235@gmail.com>
2022-03-16 18:39:21 -0700

    put GameGrid into seperate folder

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import NestedGrid from "./Components/GameGrid";
+import NestedGrid from "./Components/GameGrid/GameGrid";
 import Keyboard from "./Components/Keyboard/Keyboard";
 
 function App() {
