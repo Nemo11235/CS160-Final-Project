a2720a311a40004fca62b0f6ca7bff2ae88f1b65
Allen Liu <liuzhexian11235@gmail.com>
2022-03-19 15:49:43 -0700

    implemented router

--- wordle/src/AboutUsPage/AboutUsPage.js
+++ wordle/src/AboutUsPage/AboutUsPage.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Header from "../Components/Header";
+import "./AboutUsPage.scss";
+
 function AboutUsPage() {
   return (
-    <div className="home-style">
+    <div className="about-us-style">
       <Header />
-      <p>hihihi this is about us page</p>
+      <div className="card">
+        <h1 className="title">About us</h1>
+        <p className="content">
+          We are a group of pasionsate, driven students taught by Professor
+          Kenward Tsang. Incorporating Angile software enginnering into this
+          project has been fun, and we hope you have fun playing our version of
+          Wordle. We all learned new important skills and concepts such as
+          JavaScript, public APIs, authentication, and hope to learn more in the
+          future. Thank you for playing our Wordle game.
+        </p>
+        <img></img>
+      </div>
     </div>
   );
 }
--- wordle/src/App.js
+++ wordle/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AboutUsPage from "./AboutUsPage/AboutUsPage";
 import HomePage from "./HomePage/HomePage";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+      </Routes>
     </Router>
   );
 }
