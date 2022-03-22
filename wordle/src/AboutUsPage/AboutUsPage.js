77bdaff2ce7c54fac82425a0dfd872430554d7c4
Allen Liu <liuzhexian11235@gmail.com>
2022-03-21 20:25:36 -0700

    fixed a typo

--- wordle/src/AboutUsPage/AboutUsPage.js
+++ wordle/src/AboutUsPage/AboutUsPage.js
@@ -21,7 +21,7 @@ function AboutUsPage() {
       <div className="card">
         <h1 className="title">About us</h1>
         <h2 className="text-content">
-          We are a group of pasionsate, driven students taught by Professor
+          We are a group of passionate, driven students taught by Professor
           Kenward Tsang. Incorporating Angile software enginnering into this
           project has been fun, and we hope you have fun playing our version of
           Wordle. We all learned new important skills and concepts such as
