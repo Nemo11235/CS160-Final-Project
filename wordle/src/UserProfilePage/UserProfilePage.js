cce23ca23d0218dbf7d225cedd80087a373628b7
James Yu <jamesyu200141@gmail.com>
2022-04-04 20:29:59 -0700

    top left card

--- wordle/src/UserProfilePage/UserProfilePage.js
+++ wordle/src/UserProfilePage/UserProfilePage.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Header from "../Components/Header";
 import "./UserProfilePage.scss";
+import AccountImg from "../Images/account-img.png";
 
 
 function UserProfilePage() {
 return (
-    <div className="user-profile-style">
-    <Header />
-      <div className="card">
-        <h1 className="title">My Account</h1>
-        <div className="card-top-left">
-            
-        </div>
+  <div className="user-profile-style">
+  <Header />
+    <div className="card">
+      <h1 className="title">My Account</h1>
 
-            <div className="card-bot-left">
-
-            </div>
-
-            <div className="card-right">
-
-            </div>
+      <div className="card-top-left">
+        <img src={AccountImg} className="acc-img"/>
+        <h1 className="title2">Sample Name</h1>
+        <button className="sign-out">Sign Out</button>
+        <div className="circle"></div>
       </div>
 
+      <div className="card-bot-left">
+
+      </div>
+
+      <div className="card-right">
+
+      </div>
     </div>
+  </div>
 );
 }
 
