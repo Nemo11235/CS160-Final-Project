f9d4e817b0f950cb78e259f1bd16d53583e655fb
Allen Liu <liuzhexian11235@gmail.com>
2022-04-14 10:25:40 -0700

    set default page to homepage

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/race" element={<MultiplayerPage />} />
