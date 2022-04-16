a477f9d69cb5fd5b338e631b96f69c5623ea3d85
Allen Liu <liuzhexian11235@gmail.com>
2022-04-16 02:37:24 -0700

    update img

--- wordle/src/Components/Header/Header.js
+++ wordle/src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Logo from "../Images/logo.png";
+import Logo from "../../Images/logo.png";
 import { useNavigate } from "react-router-dom";
-import paths from "../Utils/paths";
+import paths from "../../Utils/paths";
 import "./Header.scss";
 
 function Header() {
