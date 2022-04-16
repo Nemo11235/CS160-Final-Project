5f781faac3f4ac78460d4180a90d95ea57b8ba25
terryhong4 <hongterry2114@gmail.com>
2022-04-15 17:50:39 -0700

    Added Router navigation with hamburger menu to About Us and Multiplayer Pages

--- wordle/src/AboutUsPage/AboutUsPage.js
+++ wordle/src/AboutUsPage/AboutUsPage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Components/Header/Header";
 import KenImage from "../Images/ken-img.png";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import HamburgerMenu from "../Components/HamburgerMenu/HamburgerMenu";
+import HamburgerBlur from "../Components/HamburgerMenu/HamburgerBlur";
 import { useNavigate } from "react-router-dom";
 import paths from "../Utils/paths";
 import "./AboutUsPage.scss";
@@ -13,9 +15,25 @@ function AboutUsPage() {
     navigate(paths.home);
   }
 
+  /* Hamburger Menu Implementation */
+  let [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const hamburgerOpenHandler = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+  let hamburgerMenu = <HamburgerMenu />;
+  let hamburgerBlur;
+  if (hamburgerOpen) {
+    hamburgerMenu = <HamburgerMenu
+      open={hamburgerOpen}
+      close={hamburgerOpenHandler} />;
+    hamburgerBlur = <HamburgerBlur close={hamburgerOpenHandler} />;
+  }
+
   return (
     <div className="about-us-style">
-      <Header />
+      <Header click={hamburgerOpenHandler} />
+      {hamburgerMenu}
+      {hamburgerBlur}
       <button className="back-button" onClick={goHome}>
         <ArrowBackIcon style={{ width: "97px", height: "77px" }} />
       </button>
