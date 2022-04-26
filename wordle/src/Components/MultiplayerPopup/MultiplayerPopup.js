836b94744c0ddbb93048b0d7f5d6c170a76c9acf
terryhong4 <hongterry2114@gmail.com>
2022-04-26 14:20:58 -0700

    Fixed multiplayer-popup DOM elements with the same class name leaking into other elements from other .scss files

--- wordle/src/Components/MultiplayerPopup/MultiplayerPopup.js
+++ wordle/src/Components/MultiplayerPopup/MultiplayerPopup.js
@@ -7,35 +7,34 @@ function MultiplayerPopup(props) {
     return (
         <div className="winpopup-style">
             <div className="overlay">
-                <div className="content">
-                    <div className="content-background">
-                        <button
-                            className={"exit-btn"}
-                            onClick={() => props.updateShowWinPopUp(false)}
-                        >
-                            <img src={ExitBtn} />
-                        </button>
-                        <div className="content">
+                <div className="content-background">
+                    <button
+                        className={"exit-btn"}
+                        onClick={() => props.updateShowWinPopUp(false)}
+                    >
+                        <img src={ExitBtn} />
+                    </button>
+                    <div className="content-mult">
+                        <div className="top-content">
                             {props.draw ? (
                                 <h2>Draw!</h2>
                             ) : (
                                 <h2>{props.username} has won!</h2>
                             )}
                             <p>The word was: {props.word.charAt(0)}{props.word.substring(1).toLowerCase()}</p>
-
-                            <div className="bottom-content">
-                                <div className="time-container">
-                                    <h1>Time</h1>
-                                    <p>0:01</p>
-                                </div>
-                                <div className="button-container">
-                                    <button className="rematch-btn">
-                                        Play Again
-                                    </button>
-                                    <button className="singleplayer-btn">
-                                        Singleplayer
-                                    </button>
-                                </div>
+                        </div>
+                        <div className="bottom-content">
+                            <div className="time-container">
+                                <h1>Time</h1>
+                                <p>0:01</p>
+                            </div>
+                            <div className="button-container">
+                                <button className="rematch-btn">
+                                    Play Again
+                                </button>
+                                <button className="singleplayer-btn">
+                                    Singleplayer
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -50,7 +49,7 @@ MultiplayerPopup.propTypes = {
 
 
     draw: PropTypes.bool,           // Always equal to 'false' for now
-    username: PropTypes.string,
+    username: PropTypes.string,     // Always equal to current user for now
     word: PropTypes.string,
 };
 
